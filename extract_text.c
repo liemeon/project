2c0d9c55f7ac2f4637ed5569a35be4b88bfe8fb3
Liemeon <lgh029@naver.com>
2018-12-04 12:32:42 +0900

    born make to extract text

---
+++ extract_text.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+  if(argc != 2)
+  {
+    printf("extract original_file_name");
+    exit(0);
+  }
+  //file_exist_check
+  //text file exist check
+  //system(""); //extract text
+
+  return 0;
+}
