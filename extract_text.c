e6fc0e7ee26fbfcef4b2b2f4188d2d51dbc7239b
Liemeon <lgh029@naver.com>
2018-12-05 22:33:49 +0900

    we do two api downloads and know how to use them.

--- extract_text.c
+++ extract_text.c
@@ -4,15 +4,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) // need argv only one;;
 {
   if(argc != 2)
   {
     printf("extract original_file_name");
     exit(0);
   }
-  //file_exist_check
-  //text file exist check
+  if()//file_exist_check
+  {
+
+  }
+  if()//text file exist check
+  {
+
+  }
   //system(""); //extract text
 
   return 0;
