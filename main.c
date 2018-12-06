6bbf72ac97bf09aa704bbf049ac1a49dc50b83d5
ggunnim <ggunnim@naver.com>
2018-12-06 07:40:16 -0800

    I added ticker, please see return value

--- main.c
+++ main.c
@@ -21,3 +21,20 @@ int a;
 int dir_x, dir_y;
 Snake head, tail;
 FOOD food;
+
+int set_ticker(int n_msec)
+{
+   struct itimerval timeset;
+   long n_sec, n_usec;
+
+   n_sec = n_msec / 1000;
+   n_usec = (n_msec % 1000) * 1000L;
+
+   timeset.it_interval.tv_sec = n_sec;
+   timeset.it_interval.tv_usec = n_usec;
+
+   timeset.it_value.tv_sec = n_sec;
+   timeset.it_value.tv_usec = n_usec;
+
+  return //what parameters should be?
+}
