72ce20641e9f1d1ad1d688b3091d33d16df7de9c
Liemeon <lgh029@naver.com>
2018-12-07 14:49:25 +0900

    sanke_move version 1

---
+++ snake_move.c
@@ -0,0 +1,31 @@
+void Snake_Move()
+{
+   Snake p, tmp;
+
+   for(p = tail; p != head; p = p->prev)
+  {
+    p->x = p->prev->x;
+    p->y = p->prev->y;
+   }
+   p->x += dir_x;
+   p->y += dir_y;
+
+  if(head->x > //size of x)
+  {
+    head->x = 0;
+  }
+  if(head->x < 0)
+  {
+    head->x =// size of x;
+  }
+  if(head->y > //size of y)
+  {
+    head->y = 0;
+  }
+  if(head->y < 0)
+  {
+    head->y = //size of y;
+  }
+
+  move(head->y, head->x);
+}
